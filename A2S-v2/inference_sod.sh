a179300473df5b9582da1f199c60fa730e191345
xhk <hk.xiao.me@gmail.com>
2024-10-20 22:38:04 -0400

    init

---
+++ A2S-v2/inference_sod.sh
@@ -0,0 +1,1 @@
+python3 test.py cornet --gpus=0 --weight=../pretrained/cornet_rgb_duts_tr.pth  --vals=ce --save --data_path=../data --datasets=perseg --save_path=../data
---
+++ A2S-v2/perseg_sod.sh
@@ -0,0 +1,9 @@
+python3 test.py cornet --gpus=0 \
+    --weight=../pretrained/cornet_rgb_duts_tr.pth \
+    --vals=ce --save --data_path=../data/fss-te \
+    --datasets=perseg \
+    --save_path=../data/fss-te
+
+
+
+    
