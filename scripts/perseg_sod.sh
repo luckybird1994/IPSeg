49562f26a4a8057fdefa1c2c430cf62ee732734c
xhk <hk.xiao.me@gmail.com>
2024-10-20 22:38:37 -0400

    init

---
+++ scripts/perseg_sod.sh
@@ -0,0 +1,9 @@
+python3 A2S-v2/test.py cornet --gpus=0 \
+    --weight=pretrained/cornet_rgb_duts_tr.pth \
+    --vals=ce --save --data_path=data/fss-te \
+    --datasets=perseg \
+    --save_path=data/fss-te
+
+
+
+    
